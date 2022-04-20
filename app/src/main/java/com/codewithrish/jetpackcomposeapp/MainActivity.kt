dd5ed2efbf2a364119780d660104946eae87847a
codewithrish <rishabhkumar1024@gmail.com>
2022-04-20 17:44:56 +0530

    Lazy Column

--- app/src/main/java/com/codewithrish/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/codewithrish/jetpackcomposeapp/MainActivity.kt
@@ -4,16 +4,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -32,6 +30,39 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            UserList()
+        }
+    }
+}
+
+data class User(
+    val id: Int
+)
+
+val users = listOf(
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+)
+
+@Composable
+fun UserList() {
+    /*Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
+        for (i in 1..200) {
+            UserCard()
+        }
+    }*/
+
+    LazyColumn {
+        items(users) {
             UserCard()
         }
     }
@@ -39,37 +70,50 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UserCard() {
-    Row(
+    Card(
+        elevation = 4.dp,
         modifier = Modifier
+            .padding(12.dp)
             .fillMaxWidth()
             .wrapContentHeight()
-            .padding(12.dp)
-            .border(1.dp, color = Color.Gray)
-            .padding(12.dp)
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_user),
-            contentDescription = "User Profile Image",
-            contentScale = ContentScale.Crop,
+        Row(
             modifier = Modifier
-                .size(120.dp)
-                .clip(CircleShape)
-        )
-        Column {
-            Text(text = stringResource(id = R.string.dummy_text))
-            Button(onClick = {
+                .fillMaxWidth()
+                .padding(12.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_user),
+                contentDescription = "User Profile Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape)
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.dummy_text),
+                    fontSize = 12.sp,
+                    color = Color.Gray,
+                    modifier = Modifier.padding(12.dp)
+                )
+                Button(onClick = {
 
-            }) {
-                Text(text = "View Profile")
+                }) {
+                    Text(text = "View Profile")
+                }
             }
         }
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Surface(Modifier.fillMaxSize()) {
-        UserCard()
+        UserList()
     }
 }
